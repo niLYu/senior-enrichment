4b6064344a545e59c2ca7a1786fb1bf1309d9796
Yu Lin <yu@Yus-MacBook-Pro.local>
2017-07-12 17:10:50 -0400

    added campusId

--- seed.js
+++ seed.js
@@ -1,39 +1,36 @@
 'use strict'
 
-// const Student = require('./db/models/student');
-// const Campus = require('./db/models/student')
 const db = require('./db');
-const {Student, Campus} = require('./db/models');
 
 const data = {
-  student: [
-    {name: 'John Doe', email: 'johndoe@gmail.com'},
-    {name: 'Jane Doe', email: 'janedoe@gmail.com'},
-    {name: 'Jesus Christ', email: 'jesuschrist@gmail.com'},
-    {name: 'Mother Teresa', email: 'motherteresa@gmail.com'}
-  ],
   campus: [
-    {name: 'Fullstack Academy', image: 'FSAplaceholder'},
-    {name: 'GraceHopper Academy', image: 'GHplaceholder'}
+    { name: 'Fullstack Academy', image: 'FSAplaceholder' },
+    { name: 'GraceHopper Academy', image: 'GHplaceholder' }
+  ],
+  student: [
+    { name: 'John Doe', email: 'johndoe@gmail.com', campusId: 1 },
+    { name: 'Jane Doe', email: 'janedoe@gmail.com', campusId: 2 },
+    { name: 'Jesus Christ', email: 'jesuschrist@gmail.com', campusId: 1 },
+    { name: 'Mother Teresa', email: 'motherteresa@gmail.com', campusId: 2 }
   ]
 }
 
 const dataKeys = Object.keys(data);
 
 
-db.sync({force: true})
-.then(function () {
-  console.log("Dropped old data, now inserting hard-coded data");
-  return dataKeys.map((name) => {
-    return data[name].map((item) => {
-      return db.model(name).create(item);
+db.sync({ force: true })
+  .then(function () {
+    console.log("Dropped old data, now inserting hard-coded data");
+    return dataKeys.map((name) => {
+      return data[name].map((item) => {
+        return db.model(name).create(item);
+      });
     });
-  });
-})
+  })
 
-.then(function () {
-  console.log("Finished inserting data");
-})
-.catch(function (err) {
-  console.error('Error seeding database', err);
-});
+  .then(function () {
+    console.log("Finished inserting data");
+  })
+  .catch(function (err) {
+    console.error('Error seeding database', err);
+  });
