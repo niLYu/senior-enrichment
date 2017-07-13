e3c8e591f4b841cabfc3870c80b7c4c041b03994
Yu Lin <yu@Yus-MacBook-Pro.local>
2017-07-13 12:30:35 -0400

    fixed campus list

--- app/components/CampusList.jsx
+++ app/components/CampusList.jsx
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchCampuses } from '../reducers/campuses'
 
-export default function campusList(props) {
-  const listOfCampuses = props.allCampuses.campuses;
-  return (
-    <div className="listOfCampuses">
-      <h1>List of Campuses</h1>
-      <ul>
-        {listOfCampuses && listOfCampuses.map(campus => {
-          return (
-            <li key={campus.id}>
-              <NavLink to={`/campuses/${campus.name}`}>
-                {campus.name}
-              </NavLink>
-            </li>
-            )
-          })
-        }
-      </ul>
-    </div>
-  )
+class CampusList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {campuses: []};
+  }
+
+  componentDidMount() {
+    this.props.fetchCampuses();
+  }
+
+  render() {
+
+    const listOfCampuses = this.props.campuses.campuses;
+
+    return (
+      <div className="listOfCampuses">
+        <h1>List of Campuses</h1>
+        <ul>
+          {listOfCampuses && listOfCampuses.map(campus => {
+            return (
+              <li key={campus.id}>
+                <NavLink to={`/campuses/${campus.name}`}>
+                  {campus.name}
+                </NavLink>
+              </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    )
+  }
 }
 
+const mapStateToProps = function (state) {
+  return {
+    campuses: state.campuses,
+  };
+};
+
+const mapDispatchToProps = {fetchCampuses}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CampusList);
--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -1,37 +1,25 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import store from '../store';
 import Navbar from './Navbar';
 import Home from './Home';
 import Campuses from './CampusList'
-import { fetchCampuses } from '../reducers/campuses';
-import { fetchStudents } from '../reducers/students';
 
-export default class Root extends Component {
+export default function Root() {
 
-  componentDidMount() {
-    const campusesThunk = fetchCampuses();
-    const studentsThunk = fetchStudents();
-    store.dispatch(campusesThunk);
-    store.dispatch(studentsThunk);
-  }
-
-  render() {
-    return (
-      <div>
-
-        <Router>
-          <div>
+  return (
+    <div>
+      <Router>
+        <div>
           <Navbar />
           <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route path ='/campuses' component={Campuses}/>
-            <Route component ={Home} />
+            <Route exact path='/' component={Home} />
+            <Route path='/campuses' component={Campuses} />
+            <Route component={Home} />
           </Switch>
-          </div>
-        </Router>
-      </div>
-    )
-  }
+        </div>
+      </Router>
+    </div>
+  )
+
 }
 
