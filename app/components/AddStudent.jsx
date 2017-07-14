6ac64b43fb44d5d9e74a9b6d61413f072852da61
Yu Lin <yu@Yus-MacBook-Pro.local>
2017-07-14 17:05:05 -0400

    add student working

--- app/components/AddStudent.jsx
+++ app/components/AddStudent.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {createStudent} from '../reducers/students';
+import { fetchCampuses } from '../reducers/campuses';
 
 class AddStudent extends Component {
   constructor(props) {
     super(props);
     this.state = {
         name: '',
-        email: ''
+        email: '',
+        campusId: ''
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.props.fetchCampuses();
+  }
   handleChange(event) {
     this.setState({
         [event.target.name]: event.target.value
@@ -21,9 +26,27 @@ class AddStudent extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log(this.props)
-    console.log('state', this.state);
-    this.props.createStudent(this.state);
+    let campusAssoc = 0;
+
+    for (let i = 0; i < this.props.campuses.length; i++) {
+      if (this.state.campusId == this.props.campuses[i].name) {
+        console.log(this.state.campusId == this.props.campuses[i].name)
+        campusAssoc = this.props.campuses[i].id;
+      }
+    }
+
+    if (campusAssoc) {
+      this.props.createStudent({
+        name: this.state.name,
+        email: this.state.email,
+        campusId: campusAssoc});
+    } else {
+      this.props.createStudent({
+        name: this.state.name,
+        email: this.state.email
+      })
+    }
+
     this.setState(this.state);
   }
 
@@ -48,6 +71,14 @@ class AddStudent extends Component {
               value={this.state.email}
               onChange={this.handleChange} />
           </label>
+           <label>
+            Campus:
+             <input
+              name="campusId"
+              type="text"
+              value={this.state.campusId}
+              onChange={this.handleChange} />
+          </label>
           <button
             type="submit">
             Submit
@@ -58,6 +89,12 @@ class AddStudent extends Component {
   }
 }
 
-const mapDispatchToProps = {createStudent};
+const mapStateToProps = function (state) {
+  return {
+    campuses: state.campuses
+  };
+};
 
-export default connect(null, mapDispatchToProps)(AddStudent);
+const mapDispatchToProps = {fetchCampuses, createStudent};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddStudent);
